e1936f9db1cc41fdc30b91577c84be36b99a52fb
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-07-08 22:13:30 +0530

    QRCode project source file

---
+++ app.js
@@ -0,0 +1,21 @@
+// index.js
+const QRCode = require('qrcode');
+
+const data = `https://github.com/msdhinesh45`;
+
+// Generate QR Code and print in console
+QRCode.toString(data,{type:'terminal'},function(err,url){
+    if(err) return console.error(err);
+    console.log(url);
+});
+
+// Generate QR Code as image file(PNG)
+QRCode.toFile("qrcode.png",data,{
+    color:{
+        dark:'#000',    //QR Code Color
+        light:'#fff'    //BG color
+    }
+},function(err){
+    if(err) throw err;
+console.log("QR Code as qrcode.png");
+})
